8f8215a788c085cca88364640d22596da3525400
mjhuh263 <mjhuh263@gmail.coom>
2021-03-29 21:35:57 +0900

    MODIFY: March 29th

--- march.py
+++ march.py
@@ -64,3 +64,22 @@ class Solution(object):
             	return False
                 
         return stack == []
+
+# ======================================================================================
+
+# LeetCode # 704. : Binary Search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else :
+                right = middle - 1
+        return -1
