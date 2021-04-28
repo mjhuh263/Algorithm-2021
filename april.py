6bd17df064523896b9a581126ffd948a53f61560
mjhuh263 <mjhuh263@gmail.coom>
2021-04-28 11:52:53 +0900

    MODIFY: April 28

--- april.py
+++ april.py
@@ -147,7 +147,7 @@ print(result)
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str1) == len(str2) && str1 == str2:
+        if len(str1) == len(str2) and str1 == str2:
             return str1
         elif len(str1) < len(str2):
             str1, str2 = str2, str1
