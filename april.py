8e9146e1a60b1449c7250d373875ab25208a8038
mjhuh263 <mjhuh263@gmail.coom>
2021-04-28 11:51:54 +0900

    MODIFY: April 28

--- april.py
+++ april.py
@@ -140,3 +140,19 @@ for i in range(n):
     result = max(result, min_val)
 
 print(result)
+
+# ======================================================================================
+
+# LeetCode # 1071 : Greatest Common Divisor of Strings
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if len(str1) == len(str2) && str1 == str2:
+            return str1
+        elif len(str1) < len(str2):
+            str1, str2 = str2, str1
+            return self.gcdOfStrings(str1, str2)
+        elif str1[: len(str2)] == str2:
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        else:
+            return ''
