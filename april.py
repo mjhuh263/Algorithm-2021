599f72c636c56db2f9d20608e7b7ecda8de8996d
mjhuh263 <mjhuh263@gmail.coom>
2021-04-30 15:33:02 +0900

    MODIFY: April 30

--- april.py
+++ april.py
@@ -220,3 +220,27 @@ result = 6
 -----------------------
 result = 6
 """
+
+# ======================================================================================
+
+# 이것이 코딩 테스트다 구현 # 01 : 상하좌우
+
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+dx = [0, 0, -1, -1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for plan in plans: 
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+        if nx < 1 or ny < 1 or nx > n or ny > n: # 공간을 벗어나는 경우 무시한다
+            continue
+        x, y = nx, ny
+
+print(x, y)
